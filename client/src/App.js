192360a58abfc1b16e834113acabe226981f6445
Greg P <gplechaty@gmail.com>
2021-11-30 21:56:58 -0700

    Distance Matrix successfully received; sketched out data processing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+
 import React, { useState } from 'react';
 import AddressInput from "./components/AddressInput/AddressInput.js"
 import './App.css';
 
-function App() {
 
+function App() {
+  
   const [locations, setLocations] = useState([
     {
       id: 'start',
@@ -18,7 +20,8 @@ function App() {
       address: '',
     },
   ]);
-  
+  /////////////// Location functions ///////////////
+
   function createLocation() {
     if (locations.length < 7) {
       setLocations([
@@ -45,98 +48,199 @@ function App() {
     }
   }
 
-  function calculateShortestRoute() {
+
+/////////////// Mother Function ///////////////
+
+  function generateShortestRouteDirections() {
     console.log('calc shortest route');
-    const url = constructURL(locations);
-    //console.log(url);
-    //fetch
-    getDirectionMatrix(url);
-
-
+    getDirectionMatrixData()
+    //.then(console.log('dataMatrix:', dataMatrix))
     // data into graph representation
-
     // graph impl.
-
 
   };
 
-/////////////////////////// WORKING HERE /////////////////////////
+/////////////// Getting Data ///////////////
 
-  function getShortestRoute() {
-    console.log('function: getShortestRoute')
-    //const csrftoken = getCookie('csrftoken');
-    fetch('/api/v1/directions/shortest-route/' + constructURLAddresses(locations), {
-        method: "GET",
-        mode: 'cors',
-        cache: 'no-cache',
-        //credentials: 'include',
-        headers: {
-            //'X-CSRFToken': csrftoken,
-            "Content-Type": "application/json",
-        },
-        referrerPolicy: 'no-referrer',
-        //body: locations,
-        })
-    .then(function (response) {
-        console.log(response);
-        //if (response['status'] == 200) {setloggedIn(true);}
-      })
-      .catch(function (error) {
-        console.log('Here\s the error:', error);
-      });
+function getDirectionMatrixData() {
+  const google = window.google;
+  const locationList = createLocationList(locations);
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix(
+    {
+      origins: locationList,
+      destinations: locationList,
+      travelMode: 'DRIVING',
+      //transitOptions: TransitOptions,
+      //drivingOptions: DrivingOptions,
+      //unitSystem: UnitSystem,
+      //avoidHighways: Boolean,
+      //avoidTolls: Boolean,
+    }, callback)
+    .then(console.log('take 1000:', result))
+    function callback(response, status) {
+      if (status == 'OK') {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+        let distancesOnlyMatrix = [];
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          let tempArray = [];
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.text;
+            tempArray.push(distance);
+            var duration = element.duration.text;
+            console.log('distance:', distance);
+            var from = origins[i];
+            var to = destinations[j];
+          }
+          distancesOnlyMatrix.push(tempArray);
+        }
+        //setDistanceMatrixData(results);
+        console.log(distancesOnlyMatrix);
+        return distancesOnlyMatrix;
+      } else {
+        console.log('status is not OK. status = ', status);
+      }
+    }
+    
+    
+};
+
+  function createLocationList(locations) {
+    let locationList = [];
+    for (let location of locations) {
+      locationList.push(location['address']);
+    };
+    return locationList;
+  };
+
+function callback(response, status) {
+  //console.log('results:', response)
+  if (status == 'OK') {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+    let distancesOnlyMatrix = [];
+    let tempArray = [];
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.text;
+        tempArray.push(distance);
+        var duration = element.duration.text;
+        var from = origins[i];
+        var to = destinations[j];
+      }
+      distancesOnlyMatrix.push(tempArray);
+    }
+    //setDistanceMatrixData(results);
+    return distancesOnlyMatrix;
+  } else {
+    console.log('status is not OK. status = ', status);
+  }
+}
+
+/////////////// Create Data Matrix ///////////////
+
+
+
+
+// some global variable to store the results
+var result = []
+
+// currentSize should be invoked with the array size
+function permutation(arr, currentSize) {
+    if (currentSize == 1) { // recursion base-case (end)
+        result.push(arr.join(""));
+        return result;
+    }
+    
+    for (let i = 0; i < currentSize; i++){
+        permutation(arr, currentSize - 1);
+        if (currentSize % 2 == 1) {
+            let temp = arr[0];
+            arr[0] = arr[currentSize - 1];
+            arr[currentSize - 1] = temp;
+        } else {
+            let temp = arr[i];
+            arr[i] = arr[currentSize - 1];
+            arr[currentSize - 1] = temp;
+        }
+    }
+    
+}
+
+  const array = [1,2];
+
+
+
+// use result here
+
+  
+
+      
+
+  
+
+  ////////////////////////// Example distanceObject
+
+  let distanceObject = {
+    0: {
+        name: 'waukesha, wi',
+        distances: [0, 6074, 31087]
+    },
+    1: {
+        name: 'pewaukee, wi',
+        distances: [6938, 0, 28446]
+    },
+    2: {
+        name: 'milwaukee, wi',
+        distances: [31375, 28792, 0]
+    },
 }
 
 
+let distanceArray = [
+    [0, 6074, 31087],
+    [6938, 0, 28446],
+    [31375, 28792, 0]
+]
 
+////////////////////////// Example initial array, and set of combinations
+const exampleArray = [1,2,3];
+const comboArrays = [
+    [0,1,2,3,0],
+    [0,1,3,2,0],
+    [0,2,1,3,0],
+    [0,2,3,1,0],
+    [0,3,1,2,0],
+    [0,3,2,1,0],
+]
 
+////////////////////////// function to calculate shoretest route
 
-
-
-  function constructURL(locations) {
-    let urlBeginning = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations='
-    let addresses = constructURLAddresses(locations);
-    let url = addresses + '&origins=' + addresses + '&key='
-    return addresses;
-  };
-
-  function constructURLAddresses(locations) {
-    let addresses = '';
-    for (let index in locations) {
-      if (locations[index]['id'] !== 'start') {
-        addresses = addresses + '|';
+//console.log(distanceObject[i+1][i])
+function calcShortestRoute(comboArrays) {
+  let tempSum = 0;
+  let tempDistance = 0;
+  let minDistance = Math.pow(10, 1000);
+  let minDistanceCombo = [];
+  for (let combo of comboArrays) {
+      tempSum = 0;
+      for (let i=0; i<combo.length-1; i++) {
+          tempDistance = tempDistance + distanceObject[combo[i]['distances'][i+1]];
       }
-      addresses = addresses + `${encodeURIComponent(locations[index]['address'])}`;
-    };
-    return addresses;
-  };
-  
-  function getDirectionMatrix(url) {
-    console.log('function: getDirectionMatrix', url)
-    //url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=Waukesha%2C%20WI|pewaukee%2C%20wi|milwaukee%2C%20wi&origins=Waukesha%2C%20WI|pewaukee%2C%20wi|milwaukee%2C%20wi&key=AIzaSyDjxX0fJy0Dr82LjZdOoyyb7US3XgXwECs';
-    fetch(url, {
-      method: "GET",
-      // 'Access-Control-Allow-Origin': 'http://localhost:3000/',
-      // mode: 'cors',
-      //cache: 'no-cache',
-      //credentials: 'include',
-      headers: {
-        'Access-Control-Allow-Origin': '',
-        "Content-Type": "application/json",
-    },
-
-
-      //referrerPolicy: 'no-referrer',
-      //body: activityPost,
-      })
-    .then((r) => r.json())
-    .then((data) => {
-        console.log('Got data from the Google gods!');
-        console.log('Here\'s data', data);
-
-    } )
-    .catch((e) => console.log('Boo! Something went wrong.'));
+      if (tempDistance < minDistance) {
+          minDistance = tempDistance;
+          minDistanceCombo = combo;
+      }
+      
   }
+}
 
+// return array of location names
   return (
     <div className="flex-wrap">
       <div className="flex-column">
@@ -153,8 +257,8 @@ function App() {
         onClick={createLocation}>
         Add Destination
       </div>
-      <div className="submitButton" 
-        onClick={getShortestRoute}>
+      <div className="submitButton"
+        onClick={generateShortestRouteDirections}>
         Get Shortest Route!
       </div>
       </div>
@@ -167,42 +271,3 @@ function App() {
 }
 
 export default App;
-
-/*
-class App extends Component {
-state = {
-    data: null
-  };
-
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
-      </div>
-    );
-  }
-}
-
-export default App;
-
-*/
